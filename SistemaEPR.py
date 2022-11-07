de051d161eb1795d2803df851a6467ecd037daf2
cesarsevero-gtb <direito1boab@gmail.com>
2022-11-07 20:55:00 -0300

    Arrumado bugs Ajustado o login e cadastro do sistema

--- SistemaEPR.py
+++ SistemaEPR.py
@@ -29,9 +29,33 @@ def logarCadastrar():
                 elif linha['nivel'] == 2:
                     usuarioMaster = True
                 autenticado = True
-
+                break
             else:
                 autenticado = False
+
+        if not autenticado:
+            print('E-mail ou senha incorreto')
+    elif decisao == 2:
+        print('Faça seu cadastro')
+        nome = input('Digite seu nome: ')
+        senha = input('Digite sua senha: ')
+
+        for linha in resultado:
+            if nome == linha['nome'] and senha == linha ['senha']:
+                usuarioExistente = 1
+
+        if usuarioExistente == 1:
+            print('Usuario já cadastrado tente um nome ou senha diferente')
+        elif usuarioExistente == 0:
+            try:
+                with conexao.cursor() as cursor:
+                    cursor.execute('insert into cadastros (nome, senha, nivel) values (%s, %s, %s)', (nome, senha, 1))
+                    conexao.commit()
+                print('usuario cadastrado com sucesso')
+            except:
+                print('Erro ao inserir os dados no banco de dados')
+
+    return  autenticado, usuarioMaster
 
 while not autentico:
     decisao = int(input('digite 1 para logar e 2 para se cadastrar'))
@@ -40,7 +64,12 @@ while not autentico:
         with conexao.cursor() as cursor:
             cursor.execute('select * from cadastros')
             resultado = cursor.fetchall()
-            print(resultado)
+
 
     except:
       print('erro ao se conectar ao banco de dados')
+
+    autentico, usuarioSupremo = logarCadastrar()
+
+if autentico:
+    print('autenticado')
