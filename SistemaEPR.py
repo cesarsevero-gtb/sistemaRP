60e72fe3a7e21816ec0b7e77b8a28f29ea9675ca
cesarsevero-gtb <direito1boab@gmail.com>
2022-11-09 19:13:01 -0300

    Adicionado campo de listar produto, cadastrar produto e deletar produtos

--- SistemaEPR.py
+++ SistemaEPR.py
@@ -57,6 +57,48 @@ def logarCadastrar():
 
     return  autenticado, usuarioMaster
 
+def cadastrarProduto():
+    nome = input('Digite o nome do produto')
+    ingrediente = input('Digite o nome dos ingredientes')
+    grupo = input('Categoria do produto')
+    preco = float(input('Digite o valor do produto'))
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('insert into produtos (nome, ingredientes, grupo, preco) values (%s, %s, %s, %s)',(nome, ingrediente, grupo, preco))
+            conexao.commit()
+            print('Produto cadastrado com sucesso')
+    except:
+            print('Erro ao cadastrar produto')
+
+def listarProduto():
+    produto = []
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('select * from produtos')
+            produtosCadastrado = cursor.fetchall()
+
+    except:
+        print('Erro ao se conectar ao banco de dados')
+
+    for i in produtosCadastrado:
+        produto.append(i)
+
+    if len(produto) != 0:
+        for i in range(0, len(produto)):
+            print(produto[i])
+    else:
+        print('Nenhum produto cadastrado')
+
+def excluirProduto():
+    idDeletar = int(input('Digite o ID do produtos que deseja deletar'))
+
+    try:
+        with conexao.cursor() as cursor:
+            cursor.execute('delete from produtos where id = {}'.format(idDeletar))
+    except:
+        print('erro ao excluir o produto')
+
 while not autentico:
     decisao = int(input('digite 1 para logar e 2 para se cadastrar'))
 
@@ -73,3 +115,30 @@ while not autentico:
 
 if autentico:
     print('autenticado')
+
+    if usuarioSupremo == True:
+        decisaoUsuario = 1
+
+
+
+        while decisaoUsuario != 0:
+            decisaoUsuario = int(input('Digite 0 para sair e 1 para dadastrar produto, 2 para ver produtos cadastrado'))
+
+            if decisaoUsuario == 1:
+                cadastrarProduto()
+            elif decisaoUsuario == 2:
+                listarProduto()
+
+                delete = int(input('digite 1 para deletar ou 2 para sair'))
+
+                if delete == 1:
+                    excluirProduto()
+
+    elif usuarioSupremo == False:
+        decisaoMembro = 1
+
+        while decisaoMembro !=0:
+            decisaoMembro = int(input('Digite 0 para sair ou digite 1 para ver produtos listado'))
+
+            if decisaoMembro == 1:
+                listarProduto()
